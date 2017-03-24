69241e4d31ef93beeffb1ab6ce1d08462d6e6dae
Yue <yueLife_1993@163.com>
2017-03-24 14:07:11 +0800

    1.Add PublicBundle 2.Remove AppBundle 3.Use friendsofsymfony

---
+++ src/PublicBundle/Entity/Users.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PublicBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ *
+ * @ORM\Table(name="sy_users")
+ * @ORM\Entity(repositoryClass="PublicBundle\Entity\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
